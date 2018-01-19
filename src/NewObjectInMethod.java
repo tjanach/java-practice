76d33217fdcaeae37475e6e1cb516dfdc827f1f4
Tomasz Janaszka <tomasz.janaszka@orange.com>
2018-01-19 15:31:03 +0100

    added description to the example

--- src/NewObjectInMethod.java
+++ src/NewObjectInMethod.java
@@ -1,5 +1,10 @@
 public class NewObjectInMethod {
 
+    /*
+    * this is a demo of changing the object in a method
+    * though the emp is created inside a method, the emp in main method was only changed by the first line (17)
+    *
+    * */
     public static void main(String[] args){
         NewObjectInMethod test = new NewObjectInMethod();
         Employee emp = new Employee();
